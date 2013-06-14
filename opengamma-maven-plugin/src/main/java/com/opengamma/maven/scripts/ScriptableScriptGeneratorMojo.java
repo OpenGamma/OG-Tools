f98444bc384206f809c1568f85321063ef90e2da
Stephen Colebourne <stephen@opengamma.com>
2013-06-14 17:11:30 +0100

    [PLAT-3579] Use new scripts jar file

--- opengamma-maven-plugin/src/main/java/com/opengamma/maven/scripts/ScriptableScriptGeneratorMojo.java
+++ opengamma-maven-plugin/src/main/java/com/opengamma/maven/scripts/ScriptableScriptGeneratorMojo.java
@@ -8,7 +8,6 @@ package com.opengamma.maven.scripts;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringReader;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.HashMap;
@@ -26,15 +25,13 @@ import org.apache.maven.project.MavenProject;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
-import com.opengamma.util.ClasspathUtils;
-import com.opengamma.util.annotation.ClassNameAnnotationScannerUtils;
-import com.opengamma.util.generate.scripts.Scriptable;
-import com.opengamma.util.generate.scripts.ScriptsGenerator;
+import com.opengamma.scripts.ScriptGenerator;
+import com.opengamma.scripts.ScriptUtils;
+import com.opengamma.scripts.Scriptable;
 
-import freemarker.cache.FileTemplateLoader;
-import freemarker.template.Configuration;
 import freemarker.template.Template;
 
+// CSOFF for Javadoc tags
 /**
  * Generates scripts from a template based on annotated classes.
  * 
@@ -44,73 +41,71 @@ import freemarker.template.Template;
  * @threadSafe
  * @description 
  */
+// CSON
 public class ScriptableScriptGeneratorMojo extends AbstractMojo {
+  // no underscore makes m2e auto-complete correct
 
   /**
    * Set to true to skip all processing, default false.
    * @parameter alias="skip"
    */
-  private boolean _skip;
+  private boolean skip;  // CSIGNORE
   /**
    * Where the scripts should be generated.
    * @parameter alias="outputDir" default-value="${project.build.directory}/scripts"
    * @required
    */
-  private File _outputDir;
+  private File outputDir;  // CSIGNORE
   /**
    * True to generate unix scripts, default true.
    * @parameter alias="unix" default-value="true"
    */
-  private boolean _unix;
+  private boolean unix;  // CSIGNORE
   /**
    * The basic template file name on Unix.
    * This is used as the default template file name.
    * It effectively maps to 'java.lang.Object' in the unixTemplatesMap.
-   * The default is 'com/opengamma/scripts/templates/script-template-unix.ftl'
-   * which should be on the classpath.
-   * @parameter alias="unixTemplate" default-value="com/opengamma/scripts/templates/script-template-unix.ftl"
+   * @parameter alias="unixTemplate"
    */
-  private String _unixTemplate;
+  private String unixTemplate;  // CSIGNORE
   /**
    * A map of class name to template file name on Unix.
    * @parameter alias="baseClassTemplateMap"
    */
-  private Map<String, String> _unixTemplatesMap;
+  private Map<String, String> unixTemplatesMap;  // CSIGNORE
   /**
    * True to generate windows scripts, default true.
    * @parameter alias="windows" default-value="true"
    */
-  private boolean _windows;
+  private boolean windows;  // CSIGNORE
   /**
    * The basic template file name on Windows.
    * This is used as the default template file name.
    * It effectively maps to 'java.lang.Object' in the windowsTemplatesMap.
-   * The default is 'com/opengamma/scripts/templates/script-template-windows.ftl'
-   * which should be on the classpath.
-   * @parameter alias="windowsTemplate" default-value="com/opengamma/scripts/templates/script-template-windows.ftl"
+   * @parameter alias="windowsTemplate"
    */
-  private String _windowsTemplate;
+  private String windowsTemplate;  // CSIGNORE
   /**
    * A map of class name to template file name on Windows.
    * @parameter alias="baseClassTemplateMap"
    */
-  private Map<String, String> _windowsTemplatesMap;
+  private Map<String, String> windowsTemplatesMap;  // CSIGNORE
   /**
    * Additional scripts to copy unchanged.
    * @parameter alias="additionalScripts"
    */
-  private String[] _additionalScripts;
+  private String[] additionalScripts;  // CSIGNORE
   /**
    * The project base directory.
    * @parameter default-value="${project.basedir}"
    * @required
    */
-  private File _baseDir;
+  private File baseDir;  // CSIGNORE
   /**
    * @parameter default-value="${project}"
    * @required
    */
-  private MavenProject _project;
+  private MavenProject project;  // CSIGNORE
 
   //-------------------------------------------------------------------------
   /**
@@ -123,35 +118,37 @@ public class ScriptableScriptGeneratorMojo extends AbstractMojo {
    */
   @Override
   public void execute() throws MojoExecutionException, MojoFailureException {
-    if (_skip) {
+    if (skip) {
       return;
     }
     
     // make the output directory
     try {
-      FileUtils.forceMkdir(_outputDir);
+      FileUtils.forceMkdir(outputDir);
     } catch (Exception ex) {
-      throw new MojoExecutionException("Error creating output directory " + _outputDir.getAbsolutePath(), ex);
+      throw new MojoExecutionException("Error creating output directory " + outputDir.getAbsolutePath(), ex);
     }
     
     // resolve the input
-    Map<String, String> unix = buildInputMap(_unixTemplate, _unixTemplatesMap);
-    Map<String, String> windows = buildInputMap(_windowsTemplate, _windowsTemplatesMap);
-    URL[] classpathUrls = buildProjectClasspath(_project);
+    Map<String, String> unixMap = buildInputMap(unixTemplate, unixTemplatesMap);
+    Map<String, String> windowsMap = buildInputMap(windowsTemplate, windowsTemplatesMap);
+    URL[] classpathUrls = buildProjectClasspath(project);
     ClassLoader classLoader = new URLClassLoader(classpathUrls, this.getClass().getClassLoader());
     
     // build the scripts
-    if (unix.isEmpty() == false || windows.isEmpty() == false) {
+    if (unixMap.isEmpty() == false || windowsMap.isEmpty() == false) {
       List<Class<?>> scriptableClasses = buildScriptableClasses(classpathUrls, classLoader);
       getLog().info("Generating " + scriptableClasses.size() + " scripts");
-      if (_unix) {
-        generateScripts(unix, scriptableClasses, classLoader, false);
+      if (unix) {
+        generateScripts(unixMap, scriptableClasses, classLoader, false);
       }
-      if (_windows) {
-        generateScripts(windows, scriptableClasses, classLoader, true);
+      if (windows) {
+        generateScripts(windowsMap, scriptableClasses, classLoader, true);
       }
+    } else {
+      getLog().info("No scripts to generate");
     }
-    copyAdditionalScripts();
+    copyAdditionalScripts(classLoader);
   }
 
   //-------------------------------------------------------------------------
@@ -180,13 +177,13 @@ public class ScriptableScriptGeneratorMojo extends AbstractMojo {
     } catch (DependencyResolutionRequiredException ex) {
       throw new MojoExecutionException("Error obtaining dependencies", ex);
     }
-    return ClasspathUtils.getClasspathURLs(classpathElementList);
+    return ScriptUtils.getClasspathURLs(classpathElementList);
   }
 
   //-------------------------------------------------------------------------
   // scans for the Scriptable annotation
   private static List<Class<?>> buildScriptableClasses(URL[] classpathUrls, ClassLoader classLoader) throws MojoExecutionException {
-    Set<String> scriptableClassNames = ClassNameAnnotationScannerUtils.scan(classpathUrls, Scriptable.class.getName());
+    Set<String> scriptableClassNames = ScriptUtils.findClassAnnotation(classpathUrls, Scriptable.class);
     List<Class<?>> result = Lists.newArrayList();
     for (String scriptable : scriptableClassNames) {
       result.add(resolveClass(scriptable, classLoader));
@@ -204,9 +201,9 @@ public class ScriptableScriptGeneratorMojo extends AbstractMojo {
     for (Class<?> scriptableClass : scriptableClasses) {
       Map<String, Object> templateData = new HashMap<String, Object>();
       templateData.put("className", scriptableClass);
-      templateData.put("project", _project.getArtifactId());
+      templateData.put("project", project.getArtifactId());
       Template template = lookupTempate(scriptableClass, templateMap);
-      ScriptsGenerator.generate(scriptableClass.getName(), _outputDir, template, templateData, windows);
+      ScriptGenerator.generate(scriptableClass.getName(), outputDir, template, templateData, windows);
     }
   }
 
@@ -227,29 +224,10 @@ public class ScriptableScriptGeneratorMojo extends AbstractMojo {
   // load a template
   private Template getTemplate(String templateName, ClassLoader classLoader) throws MojoExecutionException {
     try {
-      return getTemplateFromFile(_baseDir, templateName);
+      return ScriptGenerator.loadTemplate(baseDir, templateName, classLoader);
     } catch (Exception ex) {
-      try (InputStream resourceStream = classLoader.getResourceAsStream(templateName) ) {
-        if (resourceStream == null) {
-          throw new MojoExecutionException("Freemarker Template not found as a local file or resource: " + templateName);
-        }
-        String templateStr = IOUtils.toString(resourceStream);
-        Configuration cfg = new Configuration();
-        cfg.setDefaultEncoding("UTF-8");
-        return new Template(templateName, new StringReader(templateStr), cfg);
-      } catch (IOException ex2) {
-        throw new MojoExecutionException("Error loading Freemarker template: " + templateName, ex2);
-      }
+      throw new MojoExecutionException("Error loading Freemarker template: " + templateName, ex);
     }
-  }
-
-  //-------------------------------------------------------------------------
-  // loads a template from file
-  private static Template getTemplateFromFile(File baseDir, String templateFile) throws IOException {
-    Configuration cfg = new Configuration();
-    cfg.setDefaultEncoding("UTF-8");
-    cfg.setTemplateLoader(new FileTemplateLoader(baseDir));
-    return cfg.getTemplate(templateFile);
   }
 
   //-------------------------------------------------------------------------
@@ -268,26 +246,39 @@ public class ScriptableScriptGeneratorMojo extends AbstractMojo {
 
   //-------------------------------------------------------------------------
   // copies any additional scripts
-  private void copyAdditionalScripts() throws MojoExecutionException {
-    if (_additionalScripts == null || _additionalScripts.length == 0) {
+  private void copyAdditionalScripts(ClassLoader classLoader) throws MojoExecutionException {
+    if (additionalScripts == null || additionalScripts.length == 0) {
+      getLog().info("No additional scripts to copy");
       return;
     }
-    getLog().info("Copying " + _additionalScripts.length + " additional script(s)");
-    for (String script : _additionalScripts) {
-      File scriptFile = new File(script);
-      if (scriptFile.exists() == false) {
-        throw new MojoExecutionException("Additional script cannot be found: " + scriptFile);
-      }
-      if (scriptFile.isFile() == false) {
-        throw new MojoExecutionException("Additional script is not a file, directories cannot be copied: " + scriptFile);
-      }
-      try {
-        FileUtils.copyFileToDirectory(scriptFile, _outputDir);
-        File copiedFile = new File(_outputDir, scriptFile.getName());
-        copiedFile.setReadable(true, false);
-        copiedFile.setExecutable(true, false);
-      } catch (IOException ex) {
-        throw new MojoExecutionException("Unable to copy additional script: " + scriptFile, ex);
+    getLog().info("Copying " + additionalScripts.length + " additional script(s)");
+    for (String script : additionalScripts) {
+      // try file
+      File scriptFile = new File(baseDir, script);
+      File destinationFile = new File(outputDir, scriptFile.getName());
+      if (scriptFile.exists()) {
+        if (scriptFile.isFile() == false) {
+          throw new MojoExecutionException("Additional script is not a file, directories cannot be copied: " + scriptFile);
+        }
+        try {
+          FileUtils.copyFileToDirectory(scriptFile, outputDir);
+          destinationFile.setReadable(true, false);
+          destinationFile.setExecutable(true, false);
+        } catch (IOException ex) {
+          throw new MojoExecutionException("Unable to copy additional script: " + scriptFile, ex);
+        }
+      } else {
+        // then try resource
+        try (InputStream resourceStream = classLoader.getResourceAsStream(script)) {
+          if (resourceStream == null) {
+            throw new MojoExecutionException("Additional script cannot be found: " + script);
+          }
+          FileUtils.writeByteArrayToFile(destinationFile, IOUtils.toByteArray(resourceStream));
+          destinationFile.setReadable(true, false);
+          destinationFile.setExecutable(true, false);
+        } catch (IOException ex) {
+          throw new MojoExecutionException("Unable to copy additional script: " + scriptFile, ex);
+        }
       }
     }
   }
