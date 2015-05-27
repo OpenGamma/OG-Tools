a720c5786a9e7419dcdb28b0e90ef3154dbb8528
Adam Pounder <adam@opengamma.com>
2015-05-27 11:58:39 +0100

    Extract Sphinx plugin from Strata

--- gradle-sphinx-plugin/src/main/groovy/com/opengamma/tools/gradle/sphinx/SphinxPlugin.groovy
+++ gradle-sphinx-plugin/src/main/groovy/com/opengamma/tools/gradle/sphinx/SphinxPlugin.groovy
@@ -161,7 +161,8 @@ ${sphinxBuild.output.stdErr}
 		t.group = "Documentation"
 		t.description = "Package the HTML User Guide into a Zip archive"
 		t.dependsOn project.tasks[BUILD_GUIDE_TASK_NAME]
-		t.from new File(project.buildDir, "/docs/guide")
+
+		t.from project.tasks[BUILD_GUIDE_TASK_NAME]
 		t.into "html"
 
 		t.doFirst {
